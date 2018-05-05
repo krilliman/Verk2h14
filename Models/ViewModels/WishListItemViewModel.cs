fc91a2f898ab7a1146dc7fd04c1e564c0295cce7
Jesus Crist <kristmann.ingi@gmail.com>
2018-05-05 12:35:52 +0000

    Views For all the profile functionality and WishList Implemented.

---
+++ Models/ViewModels/WishListItemViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCave.Models.ViewModels
+{
+    public class WishListItemViewModel
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+    }
+}
