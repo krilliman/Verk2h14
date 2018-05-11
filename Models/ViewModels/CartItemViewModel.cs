3428acc70273d99aab2db77633da77bf796ce1bf
Unknown <kristmann.ingi@gmail.com>
2018-05-11 22:03:24 +0000

    Cart view changed

--- Models/ViewModels/CartItemViewModel.cs
+++ Models/ViewModels/CartItemViewModel.cs
@@ -5,6 +5,7 @@ namespace BookCave.Models.ViewModels
         public int Id { get; set; }
         public int BookId { get; set; }
         public string BookName { get; set; }
+        public string Image { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
         public double TotalPrice { get; set; }
